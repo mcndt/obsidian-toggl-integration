2a0053a42a5c611e33968232a218710ea3d6f3b6
Maxime Cannoodt <Maxime.Cannoodt@UGent.be>
2022-03-05 22:48:30 +0100

    WIP: implement interval cache

--- lib/toggl/TogglCache.ts
+++ lib/toggl/TogglCache.ts
@@ -1,1 +1,10 @@
-export default class TogglCache {}
+// Docs for sorted btree: https://www.npmjs.com/package/sorted-btree
+import type { Detailed } from 'lib/model/Report';
+import BTree, { ISortedMap } from 'sorted-btree';
+
+/** UNIX-time representation of a date */
+type date = number;
+
+export default class TogglCache {
+	private entries: ISortedMap<date, Detailed[]> = new BTree();
+}
